f3c13909fb60c6ea188a80e064cc6342c8979e72
S Avery <sarahashavery@gmail.com>
2022-10-10 21:38:07 -0400

    client: Home and Header base

--- client/src/components/containers/home/Home.tsx
+++ client/src/components/containers/home/Home.tsx
@@ -1,36 +1,27 @@
 import HomeStyled from "./styles";
 import { Link } from "react-router-dom";
+import { testConfig } from "../testContainer/config";
 
 const Home = () => {
   return (
     <div className="wrapper">
       <HomeStyled>
-        <div>Welcome....</div>
+        <h1>Welcome</h1>
         <div>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, totam numquam modi ullam id tempora
           molestias, iusto quibusdam quas nulla, doloremque et ut. Adipisci sequi quos consectetur officia minima
-          deserunt! Molestiae, voluptatum delectus, blanditiis labore, quidem veniam suscipit temporibus adipisci nihil
-          quis quae. Voluptatibus architecto esse corporis enim. Officiis tenetur corrupti error, odit mollitia hic
-          deserunt nulla officia laborum esse!
+          deserunt!
         </div>
         <div className="test-button-container">
-          <div id="test-one">
-            <Link to="/test/test-one" className="test-button">
-              Test One
-            </Link>
-          </div>
-          <div id="test-two">
-            <Link to="/test/test-two" className="test-button">
-              Test Two
-            </Link>
-          </div>
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, totam numquam modi ullam id tempora
-          molestias, iusto quibusdam quas nulla, doloremque et ut. Adipisci sequi quos consectetur officia minima
-          deserunt! Molestiae, voluptatum delectus, blanditiis labore, quidem veniam suscipit temporibus adipisci nihil
-          quis quae. Voluptatibus architecto esse corporis enim. Officiis tenetur corrupti error, odit mollitia hic
-          deserunt nulla officia laborum esse!
+          <ul>
+            {testConfig.map((test) => (
+              <li key={test.id} id={test.id}>
+                <Link to={`/test/${test.id}`} className="test-button">
+                  {test.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </HomeStyled>
     </div>
--- client/src/components/containers/home/styles.tsx
+++ client/src/components/containers/home/styles.tsx
@@ -3,21 +3,13 @@ import styled from "styled-components";
 const HomeStyled = styled.div`
   width: 100%;
   height: 600px;
-  background-color: lavenderblush;
+  background-color: white;
+  margin: 40px 0;
+  color: black;
 
   .test-button-container {
-    border: 1px solid black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 200px;
-  }
-  .test-button {
-    border: 1px solid black;
-    margin: 10px;
-    padding: 5px;
-    width: 100px;
-    text-align: center;
+    background: white;
+    margin: 40px;
   }
 `;
 
